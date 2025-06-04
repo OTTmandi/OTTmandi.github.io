82f77908b8b24c326d9537cf8b640045f0c0a48b
OTTmandi <meramailid22@gmail.com>
2025-06-05 02:22:06 +0530

    Reduced image sizes and shufled the pages

--- product.js
+++ product.js
@@ -6,7 +6,7 @@ const whatsappLink = document.getElementById("whatsapp-link");
 function updateLink() {
   const months = planSelect.value;
   const phone = "917667458425"; // Replace with your WhatsApp number
-  const message = `Hi, I'm interested in buying ${months} month${months > 1 ? 's' : ''} of YouTube Premium from OTT Mandi.`;
+  const message = `Hi, I'm interested in buying ${months} month${months > 1 ? 's' : ''} of Netflix Premium from OTT Mandi.`;
 
   whatsappLink.href = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 }
